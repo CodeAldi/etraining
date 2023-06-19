cefd43b8fa9feb801bfee45fe1eb05864821fa18
aldi-computer-home <codealdi@gmail.com>
2023-06-19 16:06:33 +0700

    menghilangkan login by social dan mengatur menu masing2 role

--- resources/views/layouts/dashboardPage/main.blade.php
+++ resources/views/layouts/dashboardPage/main.blade.php
@@ -71,6 +71,7 @@
                         <a href="#">St</a>
                     </div>
                     <ul class="sidebar-menu">
+                        @if (Auth::user()->getRole() == 'admin')
                         <li class="menu-header">Menu Admin</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown"><i
@@ -80,6 +81,8 @@
                                 <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                             </ul>
                         </li>
+                            
+                        @elseif(Auth::user()->getRole() == 'instruktur')
                         <li class="menu-header">Menu Instruktur</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
@@ -118,6 +121,7 @@
                                 <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
                             </ul>
                         </li>
+                        @else
                         <li class="menu-header">Menu Peserta</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i>
@@ -181,58 +185,11 @@
                                 <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
                             </ul>
                         </li>
-                        <li class="menu-header">Pages</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="auth-forgot-password.html">Forgot Password</a></li>
-                                <li><a href="auth-login.html">Login</a></li>
-                                <li><a class="beep beep-sidebar" href="auth-login-2.html">Login 2</a></li>
-                                <li><a href="auth-register.html">Register</a></li>
-                                <li><a href="auth-reset-password.html">Reset Password</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i>
-                                <span>Errors</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="errors-503.html">503</a></li>
-                                <li><a class="nav-link" href="errors-403.html">403</a></li>
-                                <li><a class="nav-link" href="errors-404.html">404</a></li>
-                                <li><a class="nav-link" href="errors-500.html">500</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i>
-                                <span>Features</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="features-activities.html">Activities</a></li>
-                                <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
-                                <li><a class="nav-link" href="features-posts.html">Posts</a></li>
-                                <li><a class="nav-link" href="features-profile.html">Profile</a></li>
-                                <li><a class="nav-link" href="features-settings.html">Settings</a></li>
-                                <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
-                                <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i>
-                                <span>Utilities</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="utilities-contact.html">Contact</a></li>
-                                <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                                <li><a href="utilities-subscribe.html">Subscribe</a></li>
-                            </ul>
-                        </li>
-                        <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i>
-                                <span>Credits</span></a></li>
+                            
+                        @endif
                     </ul>
 
-                    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-                        <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                            <i class="fas fa-rocket"></i> Documentation
-                        </a>
-                    </div>
+                    
                 </aside>
             </div>
 
